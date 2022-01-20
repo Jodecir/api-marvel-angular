7475611f4b8bb4acbf87ed5141587ade62405c2c
Jodecir Neto <jodecirneto@gmail.com>
2022-01-20 14:22:13 -0300

    UPDATE comics service with url section

--- src/app/service/comics/comic/shared/comics-api.service.ts
+++ src/app/service/comics/comic/shared/comics-api.service.ts
@@ -7,11 +7,12 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ComicsApiService {
-  WEBSITE = 'https://gateway.marvel.com:443/v1/public/comics'
+  WEBSITE = 'https://gateway.marvel.com:443/v1/public'
+  SECTION = 'comics'
   TS = "2021"
   PUBLIC_KEY = '5a237863b3cc2061003cbbc4fe20dc06';
   HASH = '04bc75e5de9e0c176cb73442c4ba0328';
-  URL_API = `${this.WEBSITE}?ts=${this.TS}&apikey=${this.PUBLIC_KEY}&hash=${this.HASH}`;
+  URL_API = `${this.WEBSITE}/${this.SECTION}?ts=${this.TS}&apikey=${this.PUBLIC_KEY}&hash=${this.HASH}`;
 
   constructor(private http: HttpClient) { }
 
