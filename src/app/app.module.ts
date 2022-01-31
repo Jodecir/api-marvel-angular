a43b30820d42222565e8c3b3f824ac1e0a28434b
Jodecir Neto <jodecirneto@gmail.com>
2022-01-31 20:55:33 -0300

    UPDATE app.module with material.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { LayoutModule } from '@angular/cdk/layout';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './shared/material/material.module';
+
 import { CharactersListComponent } from './characters/characters-list/characters-list.component';
 import { CharacterComponent } from './characters/character-card/character-card.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { ComicComponent } from './comics/comic-card/comic-card.component';
 import { ComicsListComponent } from './comics/comics-list/comics-list.component';
 import { SearchComponent } from './search/search.component';
@@ -37,13 +34,7 @@ import { SearchComponent } from './search/search.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-    MatFormFieldModule,
+    MaterialModule,
     HttpClientModule,
     FlexLayoutModule
   ],
